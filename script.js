34ec606e9c5b5c85429f1ad77932633f026711bb
alisafek <fekla@tlu.ee>
2023-05-01 21:19:45 +0300

    another js tests with queryselector

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function notavailable() {
   alert("The page is not available now, work in progress");
 }
+
+function read() {
+  document.querySelector("h4").innerHTML =
+    "The page is not available now. Sorry!";
+}
