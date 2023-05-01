742a7ed5c872d60912201093737d78339663a86c
alisafek <fekla@tlu.ee>
2023-05-01 21:30:06 +0300

    js testing

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function read() {
   document.querySelector("h4").innerHTML =
     "The page is not available now. Sorry!";
 }
+
+function readmore() {
+  if (document.querySelector("h5").innerHTML === "") {
+    document.querySelector("h5").innerHTML = "The page is not available now";
+  } else {
+    document.querySelector("h5").innerHTML = "";
+  }
+}
