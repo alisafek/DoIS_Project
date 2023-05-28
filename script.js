507de6e0565076b274d1a7c0b57ee87f83dbd52d
alisafek <fekla@tlu.ee>
2023-05-28 23:18:52 +0300

    js changes

--- script.js
+++ script.js
@@ -2,14 +2,10 @@
 
 const btnMobnavElement = document.querySelector(".btn-mobile-nav");
 const headerElement = document.querySelector(".header");
+//dot only for selectors, not for classes etc.
 btnMobnavElement.addEventListener("click", function () {
   headerElement.classList.toggle("nav-open");
 });
-
-// Close mobile navigation
-
-// if (link.classList.contains("main-nav-link"))
-//   headerElement.classList.toggle("nav-open");
 
 //Sticky navbar
 
@@ -35,12 +31,40 @@ const observer = new IntersectionObserver(
 );
 observer.observe(sectionHeroElement);
 
-// For Our work page
+// For Our work and Blog pages
 function notavailable() {
   alert("The page is not available now, work in progress");
 }
 
-// For Careers page
+// For Careers page. Modal Window
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnModalClosed = document.querySelector(".modalclosed");
+const btnModalShowed = document.querySelector(".modalshowed");
+
+// Remove and add classes
+const modalShowed = function () {
+  modal.classList.remove("hide");
+  overlay.classList.remove("hide");
+};
+
+const modalClosed = function () {
+  modal.classList.add("hide");
+  overlay.classList.add("hide");
+};
+
+btnModalShowed.addEventListener("click", modalShowed);
+btnModalClosed.addEventListener("click", modalClosed);
+overlay.addEventListener("click", modalClosed);
+
+// Listening for events everywhere; hit any key on keyboard; e parametr for event
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hide")) {
+    modalClosed();
+  }
+});
+
+// For Careers page. Text appears and disappear on vacancies
 
 function read() {
   document.querySelector("h4").innerHTML =
@@ -64,9 +88,27 @@ function readmore2() {
   }
 }
 
-// For Footer section, current year setting
-// change the text content of an element
+// For Footer section, current year setting. Change year number content of the element
 
 const footeryearElement = document.querySelector(".footeryear");
 const currentYear = new Date().getFullYear();
 footeryearElement.textContent = currentYear;
+
+//BLog page. Sound API
+var audio = document.querySelector("audio");
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function seekAudio(time) {
+  audio.currentTime = time;
+}
+
+function setVolume(volume) {
+  audio.volume = volume;
+}
